00f7e20df34cecd9db9d2e1cff05bb2c17cd8cfd
hieu1231 <hieunv174@gmail.com>
2025-07-12 14:41:40 +0700

    init app

--- src/modules/user/index.ts
+++
@@ -1,9 +0,0 @@
-// User Module - Example module structure for future features
-// This shows how to organize code by features/modules when needed
-
-// For now, just re-export main entities for reference
-export type { User } from '../../core/entities/User';
-export { UserListScreen } from '../../presentation/screens/UserListScreen';
-
-// Note: This file is not currently used in the app
-// It's here as a placeholder for future modularization
